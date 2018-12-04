53235c3c90342b20a8e6e95dc3ea71b7d3ec08ac
rattuscz <rattus.PV@gmail.com>
2018-12-04 11:17:16 +0100

    Init status

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	public Animator animator;
+
+	private bool _firing;
+
+	void Update () {
+		if ( Input.GetButton("Fire1") ) {
+			_firing = true;
+		} else {
+			_firing = false;
+		}
+	}
+
+	void FixedUpdate() {
+		animator.SetBool("IsFiring", _firing);
+	}
+}
